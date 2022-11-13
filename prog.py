088672a67ae6f64e568689cdb7f9b2f808312113
Mare Tenebrarum <m3nh3r4chan@gmail.com>
2022-11-13 03:23:51 +0300

    attempt to manage text resizing upon font change, icons

--- prog.py
+++ prog.py
@@ -16,6 +16,7 @@ if os.name == 'nt': # Only if we are running on Windows
 from tkinter import *
 from tkinter import filedialog
 from tkinter import font
+from PIL import Image, ImageTk  # sudo apt-get install python3-pil.imagetk
 
 diff_lvl = [ 'e', 't', 'a', 'o', 'i','n', 
             's', 'r', 'h', 'l', 'd', 'c', 
@@ -29,6 +30,10 @@ color_table = {'def': '#ffffff','e': '#8accd2', 't': '#d2908a', 'a': '#d1c78a', 
 'g': '#77afa1', 'w': '#4c70ac', 'y': '#53ac4c', 'b': '#86ac4c', 
 'v': '#395213', 'k': '#758162', 'x': '#619a0b', 'j': '#35755a', 
 'q': '#b6e037', 'z': '#622c2d'}
+
+# adding standard functionality Ctrl-A
+def select_all(press_key_event):
+    textbox.tag_add('sel', '1.0', 'end')
 
 def text_to_bold():
     bold_font = font.Font(textbox, textbox.cget("font"))
@@ -64,9 +69,9 @@ def rgb(hex_rgb):
 def encode_color(ch):
     if ch.lower() in diff_lvl[:5 * 5]:
         return rgb(color_table[ch.lower()]) + ch
-    else:
-        return rgb(color_table['def']) + ch
+    return ch
 
+# display names of the currently opened files
 def update_status(filepath):
     try:
         last_slash = filepath.rindex('/', 0, -1)  # exclude last posiible '\' in the end of the file path
@@ -99,14 +104,16 @@ def load_file():
 # buttons
 def clear():
     textbox.delete(1.0, END)
+    root.title('Synesthesia Trainer')
+    status_bar.config(text='')
 
 def apply_color():  # editing of text by char
     text_str = textbox.get(1.0, END)
     lines = text_str.splitlines(True)
     for line_index, line in enumerate(lines, start=1):
         for char_index, ch in enumerate(line):
-            if ch in diff_lvl[:5 * 5]:
-                color = color_table[ch]
+            if ch.lower() in diff_lvl[:5 * 5]:
+                color = color_table[ch.lower()]
                 textbox.tag_add(color, f'{line_index}.{char_index}')
     for color in color_table.values():
         textbox.tag_config(color, foreground=color)
@@ -121,39 +128,51 @@ except"""
 
 # toolbar frame
 toolbar_frame = Frame(root)
-toolbar_frame.pack(fill=X)
+toolbar_frame.pack(fill=X, side=TOP)
 
-# main frame that everything holds onto 
-main_frame = Frame(root)
-main_frame.pack(pady=5)
+# textbox frame
+text_frame = Frame(root, width=800, height=570)
+text_frame.grid_propagate(False)
+text_frame.pack(fill="both", expand=True)
+text_frame.grid_rowconfigure(0, weight=1)
+text_frame.grid_columnconfigure(0, weight=1)
+
+scroll_frame = Frame(text_frame)
+scroll_frame.grid(sticky="nw")
 
 # scrollbar init
-scroll_output = Scrollbar(main_frame, orient='vertical')
+scroll_output = Scrollbar(scroll_frame, orient='vertical')
 scroll_output.pack(side=RIGHT, fill=Y)
 
 # Text widget
-textbox = Text(main_frame, width=76, height=20, selectbackground='grey', selectforeground='white', wrap=WORD, yscrollcommand=scroll_output.set)
-textbox.pack(pady=10)
+textbox = Text(text_frame, width=76, height=20, selectbackground='grey', selectforeground='white', wrap=WORD, yscrollcommand=scroll_output.set)
+textbox.grid(row=0, column=0, sticky="nsew")
 
 # scrollbar config
 scroll_output.config(command=textbox.yview)
 
-# test buttons
-button_frame = Frame(main_frame)
-button_frame.pack()
+# configuring key shortcut
+root.bind('<Control-A>', select_all)
 
-clear_button = Button(button_frame, text='Очистить', command=clear)
-clear_button.grid(row=0, column=0)
-
-apply_button = Button(button_frame, text='Применить палитру', command=apply_color)
-apply_button.grid(row=0, column=1, padx=20)
 
 # toolbar buttons
-bold_button = Button(toolbar_frame, text='Bold', command=text_to_bold)
-bold_button.grid(row=0, column=0, sticky=W, padx=10)
+img_bold = ImageTk.PhotoImage((Image.open('./icons/b.png')).resize((50,50)))
+bold_button = Button(toolbar_frame, image=img_bold, command=text_to_bold, borderwidth=0)
+bold_button.grid(row=0, column=0, sticky=W, pady=2)
 
-italics_button = Button(toolbar_frame, text='Italics', command=text_to_italics)
-italics_button.grid(row=0, column=1, padx=1)
+img_italics = ImageTk.PhotoImage((Image.open('./icons/i.png')).resize((41,41)))
+italics_button = Button(toolbar_frame, image=img_italics, command=text_to_italics, borderwidth=0)
+italics_button.grid(row=0, column=1, pady=8)
+
+select_all_button = Button(toolbar_frame, text='Выделить все', command=lambda: select_all(True))
+select_all_button.grid(row=0, column=2)
+
+clear_button = Button(toolbar_frame, text='Очистить', command=clear)
+clear_button.grid(row=0, column=3, padx=10)
+
+img_color = ImageTk.PhotoImage((Image.open('./icons/color-palette.png')).resize((35,35)))
+apply_button = Button(toolbar_frame, image=img_color, command=apply_color, borderwidth=0)
+apply_button.grid(row=0, column=4)
 
 
 # Menu
@@ -164,18 +183,16 @@ root.config(menu=top_menu)
 menu_option_file = Menu(top_menu, tearoff=False)
 top_menu.add_cascade(label='Файл', menu=menu_option_file)
 
-menu_option_file.add_command(label='Выбрать язык')
-menu_option_file.add_command(label='Загрузить', command=load_file)
-menu_option_file.add_command(label='Применить палитру')
+menu_option_file.add_command(label='Открыть', command=load_file)
+menu_option_file.add_command(label='Создать')
 menu_option_file.add_command(label='Сохранить как...', command=save_file_as)
 menu_option_file.add_separator()
 menu_option_file.add_command(label='Выйти', command=root.quit)
 
 menu_option_palette = Menu(top_menu, tearoff=False)
 top_menu.add_cascade(label='Палитра', menu=menu_option_palette)
-menu_option_palette.add_command(label='Выбрать')
 menu_option_palette.add_command(label='Настроить')
-menu_option_palette.add_command(label='Протестировать')
+menu_option_palette.add_command(label='Выбрать')
 
 menu_option_level = Menu(top_menu, tearoff=False)
 top_menu.add_cascade(label='Уровень', menu=menu_option_level)
