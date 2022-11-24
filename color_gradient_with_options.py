bf62da116396d785ba7d1d6acb9bcd15b950c404
Mare Tenebrarum <m3nh3r4chan@gmail.com>
2022-11-24 19:51:02 +0300

    eliminate excess colors

--- color_gradient_with_options.py
+++ color_gradient_with_options.py
@@ -8,13 +8,12 @@ colormap_hex = []
 
 def generate_hex_from_theme(theme):
     cmap = mpl.colormaps[theme]
-    #if 'LinearSegmentedColormap' in type(cmap):
     cmap._init()
     rgbas = cmap._lut
     hex_arr = [mpl.colors.rgb2hex(x) for x in rgbas]
-    #else:
     for i in range(len(hex_arr) - 1):
-        colormap_hex.append(hex_arr[i])
+        if not (hex_arr[i] in [ '#ffffff', '#000000' ]):
+            colormap_hex.append(hex_arr[i])
 
 extracted_theme = sys.argv
 speed_arg = sys.argv[2]
