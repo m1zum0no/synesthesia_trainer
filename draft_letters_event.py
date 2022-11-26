ef1c528b87419cebf6931505d3db0416c3d27863
inti7ary <j129q18@gmail.com>
2022-11-26 18:18:47 +0300

    Slow down the color shift rate

--- draft_letters_event.py
+++ draft_letters_event.py
@@ -1,8 +1,17 @@
 from tkinter import Button, Tk
 from itertools import cycle
 from spectral_glow import letters_fg_color_range
+from operator import add, sub
+
+
+def get_speed_list(start_speed, end_speed, length):
+    diff = abs(end_speed - start_speed)
+    func = add if start_speed < end_speed else sub
+    return [func(start_speed, i // (length // diff)) for i in range(length)]
+
 
 rgb_iter = cycle(letters_fg_color_range)
+speed_iter = cycle(get_speed_list(5, 25, len(letters_fg_color_range)))
 
 
 def on_leave(e):
@@ -13,7 +22,7 @@ def on_leave(e):
 
 def on_enter(e):
     bt.configure(activeforeground=next(rgb_iter))
-    bt.after_ident = bt.after(15, on_enter, e)
+    bt.after_ident = bt.after(next(speed_iter), on_enter, e)
 
 
 def do_nothing():
@@ -25,7 +34,7 @@ root = Tk()
 dummy_bt = Button(root)
 default_button_bg = dummy_bt['bg']
 
-bt = Button(root, text='A', font=('Helvetica', 20, 'bold'), command=do_nothing,
+bt = Button(root, text='A', font=('Helvetica', 35, 'bold'), command=do_nothing,
             borderwidth=0,activebackground=default_button_bg, highlightbackground=default_button_bg,
             highlightthickness=0)
 bt.pack()
