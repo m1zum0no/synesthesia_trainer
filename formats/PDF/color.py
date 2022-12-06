3e528b352caae87ceb637aa839ba6ffb0770b107
m1zum0no <m3nh3r4chan@gmail.com>
2022-12-06 23:51:37 +0300

    added one line that affects nothing

--- formats/PDF/color.py
+++ formats/PDF/color.py
@@ -1,23 +1,21 @@
 import fitz
 
 def for_letter(page, text):
-    found = 0
     page_data_blocks = page.get_text('rawdict')
     for block in page_data_blocks['blocks']:
         if not block['type']:  # 0 for txt
             for line in block['lines']:
                 for span in line["spans"]:
-                    # curr_font = span['font']
+                    #curr_font = span['font']
                     # search the correct fontname  
-                    # font = fitz.Font(curr_font)
                     used_font = fitz.Font('Arial')
+                    # doc.extractFont(font) for not present 
                     for ch in span['chars']:
-                        if text == ch['c']:
+                        if ch['c'] == text:
                             letter = ch['c']
-                            selection_field = fitz.Rect(ch['bbox'])
-                            page.add_redact_annot(selection_field)
+                            selection_field = fitz.Rect(ch['bbox']) 
+                            #page.add_redact_annot(selection_field)
                             #page.apply_redactions()
-                            page.add_underline_annot(selection_field)
                             tw = fitz.TextWriter(page.rect, color=(1, 0, 0))  # color-interpolation-filters="sRGB"
                             tw.append(ch['origin'], letter, font=used_font, fontsize=span["size"])
                             tw.write_text(page)
@@ -26,6 +24,7 @@ def for_letter(page, text):
 fname = 'pdf-test.pdf'
 text = 'a'
 doc = fitz.open(fname)
+fitz.TOOLS.set_small_glyph_heights(True)
 
 for page in doc: 
     for_letter(page, text) 
