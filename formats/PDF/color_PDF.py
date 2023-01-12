b37673e59fe929d2ef87362e532a2b77609bb59e
m1zum0no <m3nh3r4chan@gmail.com>
2023-01-12 13:09:42 +0300

    fallback font draft

--- formats/PDF/color_PDF.py
+++ formats/PDF/color_PDF.py
@@ -4,17 +4,14 @@ from tables import diff_lvl, color_table
 
 # All the available fonts:
 supported_fontnames = [str(fontname) for fontname in fitz.Base14_fontdict.values()]
-for def_fontname in ("Arial", "Times", "Times Roman"):  # additional fonts  
-    supported_fontnames.append(def_fontname)
+supported_fontnames.extend([fn for fn in ("Arial", "Times", "Times Roman")])  # additional fonts
 
 # from pymupdf-fonts
 installed_fonts = [font['name'] for font in fitz.fitz_fontdescriptors.values()]
-for ext_fontname in installed_fonts:
-    supported_fontnames.append(ext_fontname)
+supported_fontnames.extend([fn for fn in installed_fonts])
 
 
-# if replace_font, ignore the level parameter and reqrite all letters in a new font 
-def for_letter(page, text):
+def for_letter(page):
     page_data_blocks = page.get_text('rawdict')
     for block in page_data_blocks['blocks']:
         if not block['type']:  # 0 for txt
@@ -22,20 +19,26 @@ def for_letter(page, text):
                 for span in line["spans"]:
                     try:  # searching for the canonic fontname 
                         doc_font = next(fontname for fontname in supported_fontnames \
-                            if fontname.casefold() in span['font'].casefold())
-                    except StopIteration:  # replace text with fallback font if none found
-                        #doc_font = 'fallback_font'
-                        replace_font = True
-                        pass
-                        # rewrite all the documnets to match the existing font
-                    the_font = fitz.Font(doc_font) 
+                                        if fontname.casefold() in span['font'].casefold())
+                        doc_font = "notos" 
+                    except StopIteration:  # replace font used in the document with a fallback font 
+                        # if it's not within the list of supported
+                        # doc_font = 'Noto Sans '  # doesn't work cause of stupid codenames
+                        # font_weight = [weight for weight in ('Bold', 'Italic') if weight in span['font']]
+                        # doc_font += ' '.join(font_weight) if font_weight else 'Regular'
+                        # 'Noto Sans Bold', 'Noto Sans Bold Italic', 'Noto Sans Italic', 'Noto Sans Regular'
+                        doc_font = "notos"
+                    # rewriting all the letters in a new font
+                    the_font = fitz.Font(doc_font)
                     for ch in span['chars']:
                         if ch['c'].lower() in diff_lvl:
                             letter = ch['c']
+                            if ch['c'] == 'o' or ch['c'] == 'a':
+                                print(color_table[ch['c']])
                             clr = Color(color_table[letter.lower()]).rgb  # convert hex value into rgb tulpe
                             rect = fitz.Rect(ch['bbox'])
-                            # scaling down the selection field
-                            # to avoid selecting surrounding letters
+                            # scaling down the letter selection field
+                            # to avoid overlap between neighbouring letters
                             selection_field = +rect 
                             selection_field.y0 += rect.height * 0.4
                             selection_field.y1 = selection_field.y0 + rect.height * 0.2
@@ -44,16 +47,29 @@ def for_letter(page, text):
                             page.apply_redactions()
                             # rewriting the letter in a new color
                             tw = fitz.TextWriter(page.rect, color=clr)
-                            tw.append(ch['origin'], letter, font=the_font, fontsize=span["size"])
+                            tw.append(ch['origin'], letter, font=the_font, fontsize=span["size"]*0.85)
                             tw.write_text(page)
-                        
 
-fname = 'pdf-test.pdf'
-text = 'a'
+
+'''
+def fonts_used_within(page):
+    # check all the fonts instances used within document
+    results = []  # list of tuples that store the information as (text, font size, font name) 
+    for page in doc:
+        dict = page.get_text("dict")
+        blocks = dict["blocks"]
+        for block in blocks:
+            if "lines" in block.keys():
+                spans = block['lines']
+                for span in spans:
+                    data = span['spans']
+                    for lines in data:
+                        results.append((lines['font']))  # lines['size']
+    print(*list(dict.fromkeys(results)), '\n')
+'''
+
+fname = 'test.pdf'
 doc = fitz.open(fname)
 fitz.TOOLS.set_small_glyph_heights(True)
-
-for page in doc: 
-    for_letter(page, text) 
-
+list(map(for_letter, doc))
 doc.save('edited-' + doc.name)
