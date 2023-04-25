f7098be029d2ff3b978da7888e136943ab5ded56
m1zum0no <m3nh3r4chan@gmail.com>
2023-04-26 00:19:17 +0300

    tests

--- formats/DJVU/ocrd/algo.sh
+++ formats/DJVU/ocrd/algo.sh
@@ -1,22 +1,41 @@
-# djvused myfile.djvu -e 'select 3; size' -- prints size of p3
-# DjVu 2479x3508, v24, 300 dpi
+###########Preparation step:###########
+# show internal layout data (chunkIDs)
+djvudump file.djvu 
 
-# show internal layout data
-djvudump les-mots-sartre-texte.djvu 
+# print size of page N, dpi for INFO
+djvused myfile.djvu -e 'select N; size'
 
-djvused les-mots-sartre-texte.djvu -e 'select 67; save-page p67.djvu' 
-# extract chunks from prev step, incl BG etc
-# djvuextract [-page=pagenum] djvufile [chkid=filename]
-djvuextract|cjb2 p67.djvu Sjbz=p67.cnk 
-# djvuextract p67.djvu Djbz=p67.djbz  
+# save page as a separate document 
+djvused file.djvu -e 'select N; save-page pN.djvu'
 
+# extract chunks from single page DJVU, including BG44 for djvumakem
+# extensions: Sjbz=.cnk, Djbz=.djbz
+# cjb2 can be used instead of djvuextract for Sjbz
+djvuextract [-page=pagenum] djvufile [chkid=filename] 
+
+
+###########Generate .txt file with characters positions:###########
 # cd ocrodjvu
-python3 ocrodjvu --save-script ../letters_positions_p67.txt -t chars -l=fra ../p67.djvu
-python3 ocrodjvu --save-script ../letters_positions_p67.txt -t chars -p=67 -l=fra ../les-mots-sartre-texte.djvu  
+python3 ocrodjvu --save-script ../out.txt -t chars -l=fra ../pN.djvu
+python3 ocrodjvu --save-script ../out.txt -t chars -p=N -l=fra ../file.djvu
 
-
-# Djbz=p67.djbz
-djvumake mod_p67.djvu Sjbz=p67.cnk FGbz=filename|#color:x,y,w,h 
+# obtain FGbz 
+djvumake edited-pN.djvu Sjbz=pN.cnk FGbz=filename|#color:x,y,w,h 
 # x,y - координаты верхего левого угла зоны
-# djvumake(m) recursively for FGbz chunk
- 
+
+# to color recursively:
+# djvumakem edited-pN.djvu INFO=,,300 INCL=?.djbz Sjbz=Sjbz.cnk FGbz_old=FGbz.cnk FGbz= BG44=BG44.cnk
+
+
+###########OpenCV and djvumakem:###########
+# generate .djvu from .pdf
+pdf2djvu -o file.djvu file.pdf
+
+# convert a page to .tiff for OpenCV 
+djvu -format=tiff -page=N file.djvu pN.tiff 
+
+# extract page; usable but might replicate references if multiple
+djvused input.djvu -e 'select N; save-page-with N.djvu' 
+
+#for encoding FGbz_old: FGbz=bzzfile
+bzz -e input output 
