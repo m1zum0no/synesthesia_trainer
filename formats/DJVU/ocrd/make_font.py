8101039d82b1308742dde5eddf457f70c9c5d530
m1zum0no <m3nh3r4chan@gmail.com>
2023-04-25 23:28:57 +0300

    tests

--- formats/DJVU/ocrd/make_font.py
+++ formats/DJVU/ocrd/make_font.py
@@ -1,9 +1,11 @@
+#glyphs.txt:
 #for font in fonts:
 #  for char, glyph in font.glyphs.items():
 #    print(char, glyph.w, glyph.h)
 
 with open('glyphs.txt', 'r') as input:
-    for line in input.readlines():
-        data = line.split(' ')
-        data[2] = data[2].strip("\n")
-        print(f'"{data[0]}": Glyph({data[1]}, {data[2]}), ')
+    with open('previous_font.py', 'a+') as output:    
+        for line in input.readlines():
+            data = line.split(' ')
+            data[2] = data[2].strip("\n")
+            output.write(f'"{data[0]}": Glyph({data[1]}, {data[2]}), ' + '\n')    
